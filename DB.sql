05b0a482580f6f7799a4cc525b5f36a33a558ccb
DESKTOP-109OD84\FORYOUCOM <powduster@naver.com>
2026-04-06 19:51:16 +0900

    DB 시간 저장 방식 수정

--- DB.sql
+++ DB.sql
@@ -1,54 +1,35 @@
--- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
---
--- Host: localhost    Database: myapp
--- ------------------------------------------------------
--- Server version	8.0.45
+use myapp;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+create table UserInfo (
+UserID varchar(50) primary key, #회원 아이디:중복불가
+UserPW varchar(255), #해쉬로 변환해서 저장
+UserName varchar(50), #닉네임:중복가능
+Email varchar(50), #이메일 문자 및 본인 확인 용도
+CreateAt TimeStamp, #가입날짜
+Balance float #지갑
+);
 
---
--- Dumping data for table `item`
---
+create table Item(
+ItemName varchar(50) primary key, #물건 이름
+UserID varchar(50),
+PostingDay TimeStamp, #물건 게시일
+ClosingDay TimeStamp, #물건 마감일
+presence bool, #물건의 유무
+price float, #가격
+Discribtion varchar(255), #설명칸
+foreign key(UserID) references UserInfo(UserID)
+);
 
-LOCK TABLES `item` WRITE;
-/*!40000 ALTER TABLE `item` DISABLE KEYS */;
-/*!40000 ALTER TABLE `item` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping data for table `trade`
---
-
-LOCK TABLES `trade` WRITE;
-/*!40000 ALTER TABLE `trade` DISABLE KEYS */;
-/*!40000 ALTER TABLE `trade` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping data for table `userinfo`
---
-
-LOCK TABLES `userinfo` WRITE;
-/*!40000 ALTER TABLE `userinfo` DISABLE KEYS */;
-/*!40000 ALTER TABLE `userinfo` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2026-03-26 21:21:17
+create table Trade(
+TradeID varchar(20), #거래번호
+ItemName varchar(50), #물품이름
+LenderID varchar(50), #빌려준 회원 아이디
+BorrowerID varchar(50), #빌려간 회원 아이디
+LendingTime TimeStamp, #빌려간 시간
+ReturningTime TimeStamp, #반납시간
+ItemReturn bool, #반납 여부
+price float, #가격
+foreign key(ItemName) references Item(ItemName),
+foreign key(LenderID) references UserInfo(UserID),
+foreign key(BorrowerID) references UserInfo(UserID)
+);
